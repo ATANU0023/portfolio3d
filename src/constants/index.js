4b6a2a575033da7083adc2658010209df1379c49
Atanu Pramanik <115628155+ATANU0023@users.noreply.github.com>
2025-04-08 08:49:15 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,15 +116,15 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Backend Developer Intern",
+    company_name: "CBMO AI",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "September 2024 - December 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Developing and maintaining web applications using Node.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Implementing REST APIs and ensuring Databases compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
